67b9a758207a7f9d8bf4bd856c27063008079c36
tigejw <184505770+tigejw@users.noreply.github.com>
2025-02-04 12:25:54 +0000

    add forms + work on functionality

--- index.js
+++ index.js
@@ -1,1 +1,58 @@
 // This will contain the JS for index.html - be sure to link to the correct file.
+
+const form = document.getElementById("input-form");
+const nameInput = document.getElementById("name");
+const topText = document.getElementById("top-text");
+const bottomText = document.getElementById("bottom-text");
+const bottomTextError = document.getElementById("bottom-text-error");
+const formContainer = document.getElementsByClassName("form-container");
+const nameError = document.getElementById("name-error");
+const topTextError = document.getElementById("top-text-error");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  console.log("submit!");
+});
+
+nameInput.addEventListener("blur", (event) => {
+  const nameValue = event.target.value;
+  if (nameValue.trim() === "") {
+    event.target.classList.add("invalid");
+    event.target.classList.remove("valid");
+    nameError.classList.add("unhidden");
+  } else {
+    event.target.classList.add("valid");
+    event.target.classList.remove("invalid");
+    nameError.classList.remove("unhidden");
+  }
+  console.log(nameValue);
+});
+
+topText.addEventListener("blur", (event) => {
+  const topTextValue = event.target.value;
+  if (topTextValue.trim() === "" || topTextValue.length >= 50) {
+    event.target.classList.add("invalid");
+    event.target.classList.remove("valid");
+    topTextError.classList.add("unhidden");
+  } else {
+    event.target.classList.add("valid");
+    event.target.classList.remove("invalid");
+    topTextError.classList.remove("unhidden");
+  }
+  console.log(topTextValue);
+});
+
+bottomText.addEventListener("blur", (event) => {
+  const bottomTextValue = event.target.value;
+  if (bottomTextValue.trim() === "" || bottomText.length >= 50) {
+    event.target.classList.add("invalid");
+    event.target.classList.remove("valid");
+    bottomTextError.classList.add("unhidden");
+  } else {
+    event.target.classList.add("valid");
+    event.target.classList.remove("invalid");
+    bottomTextError.classList.remove("unhidden");
+  }
+  console.log(bottomTextValue);
+});
